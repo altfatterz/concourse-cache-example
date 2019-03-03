b95239b58c1b5d3fe02c8bb3ec1501c4246e4c50
Zoltan Altfatter <altfatterz@gmail.com>
2019-03-03 17:57:25 +0100

    build foo-service

--- ci/tasks/foo-service.sh
+++ ci/tasks/foo-service.sh
@@ -10,5 +10,8 @@ M2_CACHE="${BUILD_FOLDER}/maven"
 echo "Generating the /.m2 symbolic link for /maven cache"
 [[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
 
+cd source-code/library
+./mvnw clean install
+
 cd source-code/foo-service
 ./mvnw clean install
