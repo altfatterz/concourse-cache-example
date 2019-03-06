edf5aa3c3227a3994157f6a448f5f3c36f2df3f6
Zoltan Altfatter <altfatterz@gmail.com>
2019-03-06 20:08:02 +0100

    test artifactory

--- ci/tasks/foo-service.sh
+++ ci/tasks/foo-service.sh
@@ -10,5 +10,5 @@ M2_CACHE="${BUILD_FOLDER}/maven"
 echo "Generating the /.m2 symbolic link for /maven cache"
 [[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
 
-cd ../foo-service
+cd source-code/foo-service
 ./mvnw clean install
